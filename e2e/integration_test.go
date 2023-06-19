8775cbafdb7ec869745cecf82b486b059fde78fd
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2023-06-19 15:19:17 +0200

    test: docker

--- e2e/integration_test.go
+++ e2e/integration_test.go
@@ -25,7 +25,7 @@ func TestWithMinikube(t *testing.T) {
 	}
 
 	mOpts := e2e.DefaultMiniKubeOpts
-	mOpts.Runtimes = []e2e.Runtime{e2e.RuntimeContainerd}
+	mOpts.Runtimes = []e2e.Runtime{e2e.RuntimeDocker}
 	//mOpts.Runtimes =e2e.AllRuntimes
 
 	e2e.WithMinikube(t, mOpts, &extFactory, []e2e.WithMinikubeTestCase{
