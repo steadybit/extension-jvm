fc2e3915f95f7ce991b6b29d0d56bb7279972a15
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2023-06-26 11:34:54 +0200

    test: e2e http client delay

--- e2e/integration_test.go
+++ e2e/integration_test.go
@@ -44,13 +44,21 @@ func TestWithMinikube(t *testing.T) {
     	Test: testDiscoverSpringBootSample,
     },
     {
-     Name: "mvc delay",
-     Test: testMvcDelay,
+    Name: "mvc delay",
+    Test: testMvcDelay,
     },
     {
-      Name: "mvc exception",
-      Test: testMvcException,
+     Name: "mvc exception",
+     Test: testMvcException,
     },
+    {
+      Name: "http client delay",
+      Test: testHttpClientDelay,
+    },
+    //{
+    // Name: "http client status",
+    // Test: testHttpClientStatus,
+    //},
   })
 }
 
@@ -216,6 +224,147 @@ func testMvcException(t *testing.T, m *e2e.Minikube, e *e2e.Extension) {
         springBootSample.AssertStatus(t, 500)
       } else {
         springBootSample.AssertStatus(t, 200)
+      }
+      require.NoError(t, action.Cancel())
+    })
+  }
+}
+
+func testHttpClientDelay(t *testing.T, m *e2e.Minikube, e *e2e.Extension) {
+  springBootSample, pid, deleteSpringBootSample := initTest(t, m, e)
+  defer deleteSpringBootSample()
+
+  tests := []struct {
+    name          string
+    delay         uint64
+    jitter        bool
+    hostAddress   string
+    expectedDelay bool
+  }{
+    {
+     name:          "should not delay http client traffic",
+     expectedDelay: false,
+    },
+    {
+      name:          "should delay http client traffic",
+      delay:         200,
+      jitter:        false,
+      expectedDelay: true,
+    },
+    {
+     name:          "should delay http client traffic on host",
+     delay:         200,
+     jitter:        false,
+     expectedDelay: true,
+     hostAddress:   "https://www.github.com",
+    },
+    {
+     name:          "should not delay http client traffic on host",
+     delay:         200,
+     jitter:        false,
+     expectedDelay: false,
+     hostAddress:   "https://steadybit.github.com",
+    },
+    {
+     name:          "should delay http client traffic with jitter",
+     delay:         200,
+     jitter:        true,
+     expectedDelay: true,
+    },
+  }
+
+  for _, tt := range tests {
+
+    if tt.hostAddress == "" {
+      tt.hostAddress = "*"
+    }
+
+    config := struct {
+      Duration int    `json:"duration"`
+      Delay    uint64 `json:"delay"`
+      Jitter   bool   `json:"delayJitter"`
+      HostAddress  string `json:"hostAddress"`
+    }{
+      Duration: 10000,
+      Delay:    tt.delay,
+      Jitter:   tt.jitter,
+      HostAddress:  tt.hostAddress,
+    }
+
+    t.Run(tt.name, func(t *testing.T) {
+      springBootSample.AssertIsReachable(t, true)
+
+      //measure customer endpoint
+      unaffectedLatency, err := springBootSample.MeasureLatency(200)
+      require.NoError(t, err, "failed to measure customers endpoint")
+
+      action, err := e.RunAction(extjvm.TargetID+".spring-httpclient-delay-attack", &action_kit_api.Target{
+        Name: "spring.application.name",
+        Attributes: map[string][]string{
+          "spring.application.name": {"spring-boot-sample"},
+          "process.pid":             {strconv.Itoa(int(pid))},
+        },
+      }, config, nil)
+      defer func() { _ = action.Cancel() }()
+      require.NoError(t, err)
+      if tt.expectedDelay {
+        springBootSample.AssertLatencyOnPath(t, unaffectedLatency+time.Duration(config.Delay)*time.Millisecond*90/100, unaffectedLatency+time.Duration(config.Delay)*time.Millisecond*350/100, "/remote/blocking?url=https://www.github.com")
+      } else {
+        springBootSample.AssertLatencyOnPath(t, 1*time.Millisecond, unaffectedLatency*2*time.Millisecond, "/remote/blocking?url=https://www.github.com")
+      }
+      require.NoError(t, action.Cancel())
+    })
+  }
+}
+
+
+func testHttpClientStatus(t *testing.T, m *e2e.Minikube, e *e2e.Extension) {
+  springBootSample, pid, deleteSpringBootSample := initTest(t, m, e)
+  defer deleteSpringBootSample()
+
+  tests := []struct {
+    name          string
+    erroneousCallRate int
+  }{
+    {
+      name:          "should not throw exceptions",
+      erroneousCallRate: 0,
+    },
+    {
+      name:          "should throw exceptions",
+      erroneousCallRate:         100,
+    },
+  }
+
+  for _, tt := range tests {
+    config := struct {
+      Duration int    `json:"duration"`
+      ErroneousCallRate    int `json:"erroneousCallRate"`
+      Pattern  string `json:"pattern"`
+      Method   string `json:"method"`
+    }{
+      Duration: 10000,
+      ErroneousCallRate:    tt.erroneousCallRate,
+      Pattern:  "/customers",
+      Method:   "GET",
+    }
+
+    t.Run(tt.name, func(t *testing.T) {
+      springBootSample.AssertIsReachable(t, true)
+
+      action, err := e.RunAction(extjvm.TargetID+".spring-mvc-exception-attack", &action_kit_api.Target{
+        Name: "spring.application.name",
+        Attributes: map[string][]string{
+          "spring.application.name": {"spring-boot-sample"},
+          "process.pid":             {strconv.Itoa(int(pid))},
+        },
+      }, config, nil)
+      defer func() { _ = action.Cancel() }()
+      require.NoError(t, err)
+      if tt.erroneousCallRate > 0 {
+        springBootSample.AssertStatusOnPath(t, 500, "/remote/blocking?url=https://www.github.com")
+      } else {
+        springBootSample.AssertStatusOnPath(t, 200, "/remote/blocking?url=https://www.github.com")
       }
       require.NoError(t, action.Cancel())
     })
--- e2e/spring_boot_sample.go
+++ e2e/spring_boot_sample.go
@@ -153,13 +153,16 @@ func (n *SpringBootSample) GetHttpStatusOfCustomersEndpoint() (int, error) {
 }
 
 func (n *SpringBootSample) MeasureLatency(expectedStatus int) (time.Duration, error) {
+  return n.MeasureLatencyOnPath(expectedStatus, "/customers")
+}
+func (n *SpringBootSample) MeasureLatencyOnPath(expectedStatus int, path string) (time.Duration, error) {
 	client, err := n.Minikube.NewRestClientForService(n.Service)
 	if err != nil {
 		return 0, err
 	}
 	defer client.Close()
 
-	response, err := client.R().Get("/customers")
+	response, err := client.R().Get(path)
 	if err != nil {
 		return 0, err
 	}
@@ -170,11 +173,14 @@ func (n *SpringBootSample) MeasureLatency(expectedStatus int) (time.Duration, er
 }
 
 func (n *SpringBootSample) AssertLatency(t *testing.T, min time.Duration, max time.Duration) {
+  n.AssertLatencyOnPath(t, min, max, "/customers")
+}
+func (n *SpringBootSample) AssertLatencyOnPath(t *testing.T, min time.Duration, max time.Duration, path string ) {
   t.Helper()
 
   measurements := make([]time.Duration, 0, 5)
   e2e.Retry(t, 8, 500*time.Millisecond, func(r *e2e.R) {
-    latency, err := n.MeasureLatency(200)
+    latency, err := n.MeasureLatencyOnPath(200, path)
     if err != nil {
       r.Failed = true
       _, _ = fmt.Fprintf(r.Log, "failed to measure package latency: %s", err)
@@ -189,6 +195,9 @@ func (n *SpringBootSample) AssertLatency(t *testing.T, min time.Duration, max ti
 
 
 func (n *SpringBootSample) ExpectedStatus(expectedStatus int) (bool, int, error) {
+  return n.ExpectedStatusOnPath(expectedStatus, "/customers")
+}
+func (n *SpringBootSample) ExpectedStatusOnPath(expectedStatus int, path string) (bool, int, error) {
   client, err := n.Minikube.NewRestClientForService(n.Service)
   if err != nil {
     return false, -1, err
@@ -205,16 +214,19 @@ func (n *SpringBootSample) ExpectedStatus(expectedStatus int) (bool, int, error)
   return true, response.StatusCode(), nil
 }
 func (n *SpringBootSample) AssertStatus(t *testing.T, expectedHttpStatus int) {
+  n.AssertStatusOnPath(t, expectedHttpStatus, "/customers")
+}
+func (n *SpringBootSample) AssertStatusOnPath(t *testing.T, expectedHttpStatus int, path string) {
   t.Helper()
 
   measurements := make([]int, 0, 5)
   e2e.Retry(t, 8, 500*time.Millisecond, func(r *e2e.R) {
-    success, statusCode, err := n.ExpectedStatus(expectedHttpStatus)
+    success, statusCode, err := n.ExpectedStatusOnPath(expectedHttpStatus, path)
     if err != nil {
       r.Failed = true
       _, _ = fmt.Fprintf(r.Log, "failed to get http status: %s", err)
     }
-    if success != true {
+    if !success {
       r.Failed = true
       measurements = append(measurements, statusCode)
       _, _ = fmt.Fprintf(r.Log, "http status %v is not expected [%d]", measurements, expectedHttpStatus)
